0c3853564f457fa638159a1ffbfeceb2cbd4cb95
eanteby <eanteby2@gmail.com>
2024-12-22 22:10:03 -0500

    initial commit

---
+++ SQLUtility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPUFramework
+{
+    public class SQLUtility
+    {
+        public static string ConnnectionString = "";
+
+        public static DataTable GetDataTable(string sqlstatement)
+        {
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = ConnnectionString;
+            conn.Open();
+            //DisplayMessage("Conn Status", conn.State.ToString());
+            var cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sqlstatement;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
